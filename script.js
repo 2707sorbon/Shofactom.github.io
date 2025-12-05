f6a254525577ca42d32b0955f6561fe21b4f4bd0
2707sorbon <2707sorbon@gmail.com>
2025-12-05 10:30:02 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Shofactom website loaded.");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector("nav ul");
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+});
